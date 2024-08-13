ba2dc12484a9833877ac1a90ebd4d968c3837ebe
H0SAM7 <hoadel2003@gmail.com>
2024-08-13 17:57:35 +0300

    "Refactored error handling: updated Failure class, added ServerFailure subclass, and modified HomeRepoImpl and HomeRepo to use new Failure class."

--- lib/features/home/data/repos/home_rebo_impl.dart
+++ lib/features/home/data/repos/home_rebo_impl.dart
@@ -1,19 +1,33 @@
 import 'package:bookly/core/errors/failuers.dart';
+import 'package:bookly/core/utils/api_services.dart';
 import 'package:bookly/features/home/data/models/book_model/book_model.dart';
 import 'package:bookly/features/home/data/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
-class HomeRepoImpl extends HomeRepo{
+class HomeRepoImpl extends HomeRepo {
   @override
-  Future<Either<Failuer, List<BookModel>>> featchAllbooks() {
-    // TODO: implement featchAllbooks
+  Future<Either<Failure, List<BookModel>>> featchAllbooks() async {
     throw UnimplementedError();
   }
 
   @override
-  Future<Either<Failuer, List<BookModel>>> featchBestSallerBooks() {
-    // TODO: implement featchBestSallerBooks
-    throw UnimplementedError();
-  }
+  Future<Either<Failure, List<BookModel>>> featchNewsBooks() async {
+    try {
+      var data = await ApiServices().getAllBooks(
+          endPoint:
+              'volumes?Filtering=free-ebooks&Sorting=newest &q=computer science');
 
+      List<BookModel> books = [];
+      for (var book in data['items']) {
+        books.add(BookModel.fromJson(book));
+      }
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioException(e));
+      }
+      return left(ServerFailure(errMessage: e.toString()));
+    }
+  }
 }
--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -3,6 +3,6 @@ import 'package:bookly/features/home/data/models/book_model/book_model.dart';
 import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
-  Future<Either<Failuer, List<BookModel>>> featchAllbooks();
-  Future<Either<Failuer, List<BookModel>>> featchBestSallerBooks();
+  Future<Either<Failure, List<BookModel>>> featchAllbooks();
+  Future<Either<Failure, List<BookModel>>> featchNewsBooks();
 }
